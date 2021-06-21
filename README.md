5bbb866bf182989f9725676710332186cea0cb14
abhinavprakash-2000 <howvabhi@gmail.com>
2021-06-21 19:15:17 +0530

    first project with only simple working

--- README.md
+++
@@ -1,3 +0,0 @@
-this is my first project
-thank you# firstproject
-# firstproject
---
+++ freecodecamp/color flipper/test.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int i=3;
+    switch (i)
+    {
+    case 0: 
+     printf(0);
+     /* constant-expression */:
+        /* code */
+        // break;
+        case 1+2:
+        printf("1");
+        case 4/2:
+        printf("2");
+    case 8%5:
+    printf("last");
+    
+    }
+    // int a[3]={1,2,3};
+    // while(a[2]==a[3])
+    //  printf("hello");
+    // int x=5,y=0;
+    // while(x-y)
+    // printf("Hi");
+    // int shifty;
+    // shifty=570;
+    // shifty=shifty>>4;
+    // shifty=shifty<<6;
+    // printf("the value of shifty is %d",shifty);
+}
